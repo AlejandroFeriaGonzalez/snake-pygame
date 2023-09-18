d9a2189797f667d4523813b9c7c1d62a829f9d7a
alejandro20019 <aferiag@unal.edu.co>
2023-09-17 23:50:39 -0500

    test

--- bloque.py
+++ bloque.py
@@ -7,43 +7,41 @@ from constantes import *
 
 class Bloque:
 
-    def __init__(self, x, y, direc):
-
+    def __init__(self, x, y):
         self.relentizacion = 10
         self.image = pygame.Surface((TILE_SIZE, TILE_SIZE))
         self.rect = self.image.get_rect()
         self.posicion = [x, y]
-
+        self.rect.topleft = mapa[(self.posicion[0], self.posicion[1])]
+        print(self.rect.topleft)
         # self.cuerpo = deque([self])
 
-        self.rect.topleft = mapa[(self.posicion[0], self.posicion[1])]
+        self.direccion = ""  # arriba, abajo, derecha, izquierda
 
-        self.direccion = direc  # arriba, abajo, derecha, izquierda
+    # def update(self) -> None:
+    #     if self.direccion == "arriba":
+    #         self.posicion[1] -= 1
+    #     elif self.direccion == "abajo":
+    #         self.posicion[1] += 1
+    #     elif self.direccion == "izquierda":
+    #         self.posicion[0] -= 1
+    #     elif self.direccion == "derecha":
+    #         self.posicion[0] += 1
+    #     # verifica si salio del mapa
+    #     if (self.posicion[0] < 1 or self.posicion[1] < 1
+    #             or self.posicion[0] > WINDOWS // TILE_SIZE or self.posicion[1] > WINDOWS // TILE_SIZE):
+    #         pygame.quit()
+    #         exit()
+    #
+    #     pygame.draw.rect(screen, "green", self.rect)
 
-    def update(self) -> None:
-        if self.direccion == "arriba":
-            self.posicion[1] -= 1
-        elif self.direccion == "abajo":
-            self.posicion[1] += 1
-        elif self.direccion == "izquierda":
-            self.posicion[0] -= 1
-        elif self.direccion == "derecha":
-            self.posicion[0] += 1
-        # verifica si salio del mapa
-        if (self.posicion[0] < 1 or self.posicion[1] < 1
-                or self.posicion[0] > WINDOWS // TILE_SIZE or self.posicion[1] > WINDOWS // TILE_SIZE):
-            pygame.quit()
-            exit()
+    # self.cuerpo.appendleft(Bloque(*self.posicion))
+    # # print([c.rect for c in self.cuerpo])
+    #
+    # if self.cuerpo[0].posicion == [2, 2]:
+    #     pass
+    # else:
+    #     self.cuerpo.pop()
 
-        pygame.draw.rect(screen, "green", self.rect)
-
-        # self.cuerpo.appendleft(Bloque(*self.posicion))
-        # # print([c.rect for c in self.cuerpo])
-        #
-        # if self.cuerpo[0].posicion == [2, 2]:
-        #     pass
-        # else:
-        #     self.cuerpo.pop()
-
-        # for parte in self.cuerpo:
-        #     pygame.draw.rect(screen, "green", parte.rect)
+    # for parte in self.cuerpo:
+    #     pygame.draw.rect(screen, "green", parte.rect)
--- snake.py
+++ snake.py
@@ -9,7 +9,7 @@ from constantes import *
 
 clock = pygame.time.Clock()
 # culebra = pg.sprite.Group()
-bloq = Bloque(randrange(*RANGE), randrange(*RANGE), "")
+bloq = Bloque(randrange(*RANGE), randrange(*RANGE))
 
 # cola = bloque.Bloque('green', culebra)
 
@@ -17,45 +17,60 @@ manzana = pg.sprite.GroupSingle()
 
 
 def main():
+    global nuevox, nuevoy
     desbloqueado = True
     cuerpo = deque([bloq])
-    direccion = ""
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 exit()
             if event.type == pg.KEYDOWN:
-                if desbloqueado:
-                    for parte in cuerpo:
-                        if event.key == pygame.K_UP and parte.direccion != "abajo":
-                            print("arriba")
-                            direccion = 'arriba'
-                        elif event.key == pygame.K_DOWN and parte.direccion != "arriba":
-                            direccion = 'abajo'
-                        elif event.key == pygame.K_LEFT and parte.direccion != "derecha":
-                            direccion = 'izquierda'
-                        elif event.key == pygame.K_RIGHT and parte.direccion != "izquierda":
-                            direccion = 'derecha'
-                        desbloqueado = False
+                if event.key == pygame.K_UP and cuerpo[0].direccion != "abajo":
+                    cuerpo[0].direccion = 'arriba'
+                elif event.key == pygame.K_DOWN and cuerpo[0].direccion != "arriba":
+                    cuerpo[0].direccion = 'abajo'
+                elif event.key == pygame.K_LEFT and cuerpo[0].direccion != "derecha":
+                    cuerpo[0].direccion = 'izquierda'
+                elif event.key == pygame.K_RIGHT and cuerpo[0].direccion != "izquierda":
+                    cuerpo[0].direccion = 'derecha'
 
         screen.fill('black')  # limpia pantalla
         for i in range(0, WINDOWS, TILE_SIZE):
             for j in range(0, WINDOWS, TILE_SIZE):
                 pg.draw.rect(screen, 'white', (i, j, TILE_SIZE, TILE_SIZE), 1)
 
-        for parte in cuerpo:
-            parte.update()
+        if cuerpo[0].direccion == "arriba":
+            nuevoy = cuerpo[0].posicion[1] - 1
+            nuevox = cuerpo[0].posicion[0]
+        elif cuerpo[0].direccion == "abajo":
+            nuevoy = cuerpo[0].posicion[1] + 1
+            nuevox = cuerpo[0].posicion[0]
+        elif cuerpo[0].direccion == "izquierda":
+            nuevox = cuerpo[0].posicion[0] - 1
+            nuevoy = cuerpo[0].posicion[0]
+        elif cuerpo[0].direccion == "derecha":
+            nuevox = cuerpo[0].posicion[0] + 1
+            nuevoy = cuerpo[0].posicion[0]
+        else:
+            nuevox = cuerpo[0].posicion[0]
+            nuevoy = cuerpo[0].posicion[0]
+        # verifica si salio del mapa
+        if (cuerpo[0].posicion[0] < 1 or cuerpo[0].posicion[1] < 1
+                or cuerpo[0].posicion[0] > WINDOWS // TILE_SIZE or cuerpo[0].posicion[1] > WINDOWS // TILE_SIZE):
+            pygame.quit()
+            exit()
 
-        cuerpo.appendleft(Bloque(cuerpo[0].posicion[0], cuerpo[0].posicion[1], direccion))
+        cuerpo.appendleft(Bloque(nuevox, nuevoy))
         if cuerpo[0].posicion == [2, 2]:
             pass
         else:
             cuerpo.pop()
 
+        for parte in cuerpo:
+            pygame.draw.rect(screen, "green", parte.rect)
         pg.display.flip()
 
         clock.tick(FPS)
-        desbloqueado = True
 
 
 if __name__ == '__main__':
